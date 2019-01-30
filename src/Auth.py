9580567479344b1050d66360f28a8a95ada6e48b
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2019-01-30 18:01:08 +0100

    Update Auth.py

--- src/Auth.py
+++ src/Auth.py
@@ -4,14 +4,14 @@ import requests
 headers = {'Content-Type': 'application/json'}
 
 def get_member_info(uid):
-    body = {'username': 'makerapi', 'password': '***REMOVED***'}
+    body = {'username': 'makerspace_api', 'password': 'makerspace_password'}
 
-    r = requests.post('http://***REMOVED***/auth', data = json.dumps(body), headers=headers)
+    r = requests.post('http://ADDRESS/auth', data = json.dumps(body), headers=headers)
 
     access_token = r.json()['access_token']
 
     auth_headers = {'Authorization': 'JWT ' + access_token}
 
-    r = requests.get('http://***REMOVED***/user', headers=auth_headers, params={'uid': uid})
+    r = requests.get('http://ADDRESS/user', headers=auth_headers, params={'uid': uid})
 
     return r.json()
