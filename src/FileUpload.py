61dd350957a30de8d08915424bb0c5c07f9dc3b3
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2019-01-30 17:59:41 +0100

    Update FileUpload.py

--- src/FileUpload.py
+++ src/FileUpload.py
@@ -5,7 +5,7 @@ BUCKET_NAME="wacky-assets"
 
 class FileUpload():
     def __init__(self): 
-       self.s3 = boto3.client("s3", config=Config(signature_version='s3v4'), aws_access_key_id="AKIAJ2SDNGMYJM4CL3OQ", aws_secret_access_key="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") # needs to use s3v4 because germany is secure ;)
+       self.s3 = boto3.client("s3", config=Config(signature_version='s3v4'), aws_access_key_id="ACCESS_KEY_ID", aws_secret_access_key="SECRET_ACCESS_KEY") # needs to use s3v4 because germany is secure ;)
        print(self.s3.list_buckets())
 
     def upload_file(self, name, content):
